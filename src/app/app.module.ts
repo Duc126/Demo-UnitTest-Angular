b1dc9386bcdcf2e5f85a60b661fff6cf59713aa0
Bui Dinh Duc <ducbui1211@gmail.com>
2023-09-05 11:14:29 +0700

    Exercise 1: Testing a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule,
 
   ],
   providers: [],
